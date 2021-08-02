9418817caf533b90de8ffeb239dfbaa51d82d827
Max Gabriel <88243906+MaxGabrielima@users.noreply.github.com>
2021-08-02 17:38:00 -0300

    Correção de erro gramatical.

--- desafio014.py
+++ desafio014.py
@@ -1,3 +1,3 @@
 c = float(input('Digite a temperatura em C°: '))
 f = c * 1.8 + 32
-print('{:.0f} C° é igual a {:.0f}° Fahrenheight'.format(c, f))
+print('{:.0f} C° é igual a {:.0f}° Fahrenheit'.format(c, f))
