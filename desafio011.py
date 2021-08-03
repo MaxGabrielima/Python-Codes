a150736cf2d2fa18d9bde52a927920be40da139d
Max Gabriel <88243906+MaxGabrielima@users.noreply.github.com>
2021-08-03 20:21:08 +0000

    Delete desafio011.py

--- desafio011.py
+++
@@ -1,7 +0,0 @@
-L = float(input('Largura em Metros: '))
-A = float(input('Altura em Metros: '))
-R = float(input('Rendimento Fabricante: '))
-D = int(input('Demãos: '))
-a = L * A
-Q = a * D / R
-print('A área é de {} metros quadrados e será(ão) necessário(s) {} demão(s), então você precisará de {} litros de tinta'.format(a, D, Q))
