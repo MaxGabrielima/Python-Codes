0e4305794a03c1401f8f554e7937859d98ecd0e0
Max Gabriel <88243906+MaxGabrielima@users.noreply.github.com>
2021-08-06 15:55:04 -0300

    Att

--- Desafios/desafio016.py
+++ Desafios/desafio016.py
@@ -4,4 +4,4 @@ f = float(input('Digite um número: '))
 r= math.floor(f)
 print('A porção inteira de {} é {}'.format(f, r))
 
-#também é possível resolver utilizando o comando trunc
+#também é possível resolver utilizando o comando trunc também do módulo math, ou a função int que é interna do Python
